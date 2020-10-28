8dfb74fb861f595a06030a66dd81bffa5ae32e69
RoyTrenneman79 <73612890+RoyTrenneman79@users.noreply.github.com>
2020-10-28 20:50:51 +0100

    code refactoring

--- sorting.js
+++ sorting.js
@@ -1,5 +1,7 @@
 function sortNet10(data) {    // ten-input sorting network by Waksman, 1969
+    
     var swap;
+    
     if (data[0] > data[5]) { swap = data[0]; data[0] = data[5]; data[5] = swap; }
     if (data[1] > data[6]) { swap = data[1]; data[1] = data[6]; data[6] = swap; }
     if (data[2] > data[7]) { swap = data[2]; data[2] = data[7]; data[7] = swap; }
@@ -29,11 +31,12 @@ function sortNet10(data) {    // ten-input sorting network by Waksman, 1969
     if (data[6] > data[7]) { swap = data[6]; data[6] = data[7]; data[7] = swap; }
     if (data[3] > data[4]) { swap = data[3]; data[3] = data[4]; data[4] = swap; }
     if (data[5] > data[6]) { swap = data[5]; data[5] = data[6]; data[6] = swap; }
+    
     return(data);
 }
 
 alert(sortNet10([5,7,1,8,4,3,6,9,2,0]));
-
+// DCTF{Delete_passwords_tokens_and_keys_before_uploading_to_git_server}
 alert(sortNet10([6,7,1,8,4,5,3,9,2,0]));
 
 alert(sortNet10([5,7,6,9,2,0,1,8,4,3]));
